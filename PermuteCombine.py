bd975f6641267c5b008de619d258f894789ff08b
Mogei Wang <mogeiwang@gmail.com>
2016-01-28 01:01:12 +0800

    Update PermuteCombine.py

--- PermuteCombine.py
+++ PermuteCombine.py
@@ -35,25 +35,25 @@
 https://docs.python.org/2/library/itertools.html
 """
 
+from decimal import *
+getcontext().prec = 10
+
 def StirlingFactorial(n):
-    "Stirling's approximation for factorials"
-    return (n/e)**n * sqrt(2*pi*n)
+    "Stirling's approximation for factorials (NOT used for overflow issue)"
+    return Decimal((n/e)**n) * Decimal(sqrt(2*pi*n))
 
-def Factorial(n, appr=True):
-    if appr:
-        return StirlingFactorial(n)
-    else:
-        return math.factorial(n)
+def Factorial(n):
+    return Decimal(math.factorial(n))
 
-def CountPermutations(n, r, appr=True):
-    if n == r: return  1.0 * Factorial(n,appr)
-    if r == 0: return 1
-    return 1.0 * Factorial(n,appr) / Factorial(n-r,appr)
+def CountPermutations(n, r):
+    if n == r: return Decimal(1.0) * Factorial(n)
+    if r == 0: return Decimal(1.0)
+    return Decimal(1.0) * Factorial(n) / Factorial(n-r)
 
-def CountCombinations(n, r, appr=True):
-    if n == r: return 1
-    if r == 0: return 1
-    return (1.0 * Factorial(n,appr)) / (Factorial(r,appr) * Factorial(n-r,appr))
+def CountCombinations(n, r):
+    if n == r: return Decimal(1.0)
+    if r == 0: return Decimal(1.0)
+    return (Decimal(1.0) * Factorial(n)) / (Factorial(r) * Factorial(n-r))
 
 """
 TEST:
@@ -61,12 +61,11 @@ TEST:
      & the probility that a PN and KC is connected is quite close to 0.5
 """
 
-'''
-PN_num = 80 # 830
-KC_num = 5000 # 50000
+PN_num = 830
+KC_num = 50000
 PN_feedin_num = PN_num/2
 
-def relyPN_to_KC(x=PN_feedin_num, k=10, rely=10):
+def relyPN_to_KC(x=PN_feedin_num, k=80, rely=100):
     """compute the probility that all reliable PNs are connected to a given KC:
     >>> x is total pick up PN number,
     >>> k is picked up reliable PN number,
@@ -74,51 +73,55 @@ def relyPN_to_KC(x=PN_feedin_num, k=10, rely=10):
     """
     return CountCombinations(rely, k) * CountCombinations(PN_num-rely, x-k) / CountCombinations(PN_num, x)
 
-rely = 8
-l1=[]
+"""
+suppose there are 800 reliable PNs in total, and we randomly select PN_feedin_num PNs to connect to a given KC,
+then, how many reliable PNs would be in the selected PN set?
+"""
+rely = 80
+l1 = []
 print "\nThere are", rely, "reliable PNs:"
-for k in range(0, rely+1, 1):
-    ttt=relyPN_to_KC(PN_feedin_num, k, rely)
+for k in range(0, rely+1, 10):
+    ttt = relyPN_to_KC(PN_feedin_num, k, rely)
     print k, ttt
     l1.append(ttt)
 
-rely = 9
-l2=[]
+rely = 90
+l2 = []
 print "\nThere are", rely, "reliable PNs:"
-for k in range(0, rely+1, 1):
-    ttt=relyPN_to_KC(PN_feedin_num, k, rely)
+for k in range(0, rely+1, 10):
+    ttt = relyPN_to_KC(PN_feedin_num, k, rely)
     print k, ttt
     l2.append(ttt)
 
-rely = 10
-l3=[]
+rely = 100
+l3 = []
 print "\nThere are", rely, "reliable PNs:"
-for k in range(0, rely+1, 1):
-    ttt=relyPN_to_KC(PN_feedin_num, k, rely)
+for k in range(0, rely+1, 10):
+    ttt = relyPN_to_KC(PN_feedin_num, k, rely)
     print k, ttt
     l3.append(ttt)
 
-rely = 11
-l4=[]
+rely = 110
+l4 = []
 print "\nThere are", rely, "reliable PNs:"
-for k in range(0, rely+1, 1):
-    ttt=relyPN_to_KC(PN_feedin_num, k, rely)
+for k in range(0, rely+1, 10):
+    ttt = relyPN_to_KC(PN_feedin_num, k, rely)
     print k, ttt
     l4.append(ttt)
 
-rely = 12
-l5=[]
+rely = 120
+l5 = []
 print "\nThere are", rely, "reliable PNs:"
-for k in range(0, rely+1, 1):
-    ttt=relyPN_to_KC(PN_feedin_num, k, rely)
+for k in range(0, rely+1, 10):
+    ttt = relyPN_to_KC(PN_feedin_num, k, rely)
     print k, ttt
     l5.append(ttt)
 
-rely = 13
-l6=[]
+rely = 130
+l6 = []
 print "\nThere are", rely, "reliable PNs:"
-for k in range(0, rely+1, 1):
-    ttt=relyPN_to_KC(PN_feedin_num, k, rely)
+for k in range(0, rely+1, 10):
+    ttt = relyPN_to_KC(PN_feedin_num, k, rely)
     print k, ttt
     l6.append(ttt)
 
@@ -130,6 +133,13 @@ plot(l4)
 plot(l5)
 plot(l6)
 
-tttt=[l1[-1], l2[-1], l3[-1], l4[-1], l5[-1], l6[-1]]
-figure()
-plot(tttt)  '''
+def PS(x, lamda):
+    "Poisson distribution density with parameter lamda at the point x"
+    return math.exp(-lamda)*(lamda**x)/math.factorial(x)
+
+"Poisson distribution can be used to approxmiate binary distribution"
+for i in range(100):
+    print i, PS(i, KC_num*0.00001)
+#                           \--- this probablity is printed above,
+#                                choose the correct one， and note
+#                                range(100) may also need to be fixed
